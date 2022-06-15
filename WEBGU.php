ba4e470c68b95676c2c831759da35ac2aac72307
Yada Suwannarat <99582810+Yadasuwannarat@users.noreply.github.com>
2022-06-15 11:26:54 +0700

    Add files via upload

---
+++ WEBGU.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WEBGU</title>
+    <link rel="stylesheet" href="WEBGU.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+</head>
+<body>
+  <div class="container">
+      
+      <!-- เพิ่มข้อมูลทุกอย่างลงใน สกรีน -->
+    <div class="screen">
+        <div class="NIXX">
+         NIXX   
+        </div>
+        <form action="" class="login">
+            <div class="login_feeld">
+                <input type="text" placeholder="User">
+            </div>
+        </form>
+    </div>
+  </div>  
+</body>
+</html>
